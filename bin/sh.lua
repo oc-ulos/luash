a9a727af2afea16a82c6cd0275061838d48de7b1
Ocawesome101 <ocawesome101@gmail.com>
2022-04-08 10:46:26 -0400

    sh is just a thin wrapper over the lua prompt

--- bin/sh.lua
+++ bin/sh.lua
@@ -1,3 +1,9 @@
--- sh: Simple LuaSH-based shell
+-- sh: Simple LuaSH-based shell.
+-- doesn't actually contain much logic; lets the Lua REPL do most of the work
 
+local sh = require("sh")
+local sys = require("sys")
 
+local cmdline = {[0] = "luash", "-i", "-l", "sh", "/etc/profile.lua"}
+
+sys.exec(sh.resolve("lua"), cmdline)
