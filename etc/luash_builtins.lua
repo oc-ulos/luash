ac61cd676b03aaaf09ddee8dc83a93f92d5cd228
Ocawesome101 <ocawesome101@gmail.com>
2022-04-18 14:59:00 -0400

    a couple small utility things

--- etc/luash_builtins.lua
+++ etc/luash_builtins.lua
@@ -5,6 +5,8 @@ local sys = require("syscalls")
 local sh = require("sh")
 function sh.cd(dir)
   sys.chdir(dir or os.getenv("HOME") or "/")
+  sh._PS1 = sys.getcwd().."> "
 end
 sh.pwd = sys.getcwd
 sh.echo = print
+sh._PS1 = sys.getcwd().."> "
